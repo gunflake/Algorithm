6aae9c40ba19dcc10a236d821b962965a405e3d4
NamHyukMin <gunflake09@naver.com>
2018-07-28 14:26:49 +0900

    1 problem solve
    Leetcode P855 (ExamRoom) solved

--- src/leetcode/P855.java
+++ src/leetcode/P855.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.*;
+
 public class P855 {
     public static void main(String[] args) {
         ExamRoom r = new ExamRoom(10);
@@ -15,51 +17,70 @@ public class P855 {
 }
 
 class ExamRoom {
-    static boolean[] check;
-    static int size;
-    static int N;
+
+    private int size;
+    private TreeSet<Integer> set;
+    private int N;
 
     public ExamRoom(int N) {
-        check = new boolean[N];
+        set = new TreeSet<>();
         size = 0;
         this.N = N;
     }
 
     public int seat() {
-        if(size==0){ // empty class
-            check[0]=true;
+        if (size == 0) {
+            set.add(0);
+            size++;
             return 0;
-        }else{
-
-            int count = 0;
-            int max = 0;
-            int before = -1;
-
-            for (int i = 0; i < N; i++) {
-                if(check[i]){ // true
-
-                    if(count%2==1){
-
-                    }else{
-
-                    }
-
-
-                }else{ //false
-                    count++;
+        } else if (size == 1) {
+            Iterator<Integer> it = set.iterator();
+            int tmp = it.next();
+            size++;
+            if (N - tmp - 1 > tmp) {
+                set.add(N - 1);
+                return N - 1;
+            } else {
+                set.add(0);
+                return 0;
+            }
+        } else {
+            Iterator<Integer> it = set.descendingIterator();
+            int end = it.next();
+            it = set.iterator();
+            int start = it.next();
+            int length = 0;
+            int before = start;
+            int savePosition = 0;
+            while (it.hasNext()) {
+                int tmp = it.next();
+                if ((tmp - before) / 2 > length) {
+                    length = (tmp - before) / 2;
+                    savePosition = (tmp + before) / 2;
                 }
+                before = tmp;
             }
 
-
+            if(start>= length && start>=N-1-end){
+                set.add(0);
+                size++;
+                return 0;
+            }
+            if(N-1-end >length && N-1-end>start){
+                set.add(N-1);
+                size++;
+                return N-1;
+            }
+            set.add(savePosition);
+            size++;
+            return savePosition;
 
         }
-
-
     }
 
     public void leave(int p) {
+        set.remove(p);
         size--;
-        check[p] = false;
     }
 }
 
